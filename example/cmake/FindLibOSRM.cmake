d778060e3a791cf13273402e5702017c02c16c03
Dane Springmeyer <dane@mapbox.com>
2016-05-25 16:31:04 -0400

    fix various pkg-config usage

--- example/cmake/FindLibOSRM.cmake
+++ example/cmake/FindLibOSRM.cmake
@@ -1,13 +1,24 @@
 # - Try to find LibOSRM
 # Once done this will define
 #  LibOSRM_FOUND - System has LibOSRM
-#  LibOSRM_INCLUDE_DIRS - The LibOSRM include directories
 #  LibOSRM_LIBRARIES - The libraries needed to use LibOSRM
-#  LibOSRM_DEFINITIONS - Compiler switches required for using LibOSRM
+#  LibOSRM_LIBRARY_DIRS - The libraries paths needed to find LibOSRM
+#  LibOSRM_CXXFLAGS - Compiler switches required for using LibOSRM
 
 find_package(PkgConfig)
-pkg_check_modules(PC_LibOSRM QUIET libosrm)
-set(LibOSRM_DEFINITIONS ${PC_LibOSRM_CFLAGS_OTHER})
+pkg_search_module(PC_LibOSRM QUIET libosrm)
+
+function(JOIN VALUES GLUE OUTPUT)
+  string (REPLACE ";" "${GLUE}" _TMP_STR "${VALUES}")
+  set (${OUTPUT} "${_TMP_STR}" PARENT_SCOPE)
+endfunction()
+
+JOIN("${PC_LibOSRM_CFLAGS}" " " output)
+#MESSAGE("${output}") # will output "a:b:c"
+
+set(LibOSRM_CXXFLAGS ${output})
+set(LibOSRM_LIBRARY_DIRS ${PC_LibOSRM_LIBRARY_DIRS})
+message(STATUS "static ${PC_LibOSRM_STATIC_LDFLAGS}")
 
 find_path(LibOSRM_INCLUDE_DIR osrm/osrm.hpp
   PATH_SUFFIXES osrm include/osrm include
@@ -18,8 +29,6 @@ find_path(LibOSRM_INCLUDE_DIR osrm/osrm.hpp
   /usr
   /opt/local
   /opt)
-
-set(LibOSRM_INCLUDE_DIRS ${LibOSRM_INCLUDE_DIR} ${LibOSRM_INCLUDE_DIR}/osrm)
 
 find_library(TEST_LibOSRM_STATIC_LIBRARY Names osrm.lib libosrm.a
   PATH_SUFFIXES osrm lib/osrm lib
@@ -42,9 +51,9 @@ find_library(TEST_LibOSRM_DYNAMIC_LIBRARY Names osrm.dynlib libosrm.so
 
 if (NOT ("${TEST_LibOSRM_STATIC_LIBRARY}" STREQUAL "TEST_LibOSRM_STATIC_LIBRARY-NOTFOUND"))
   if ("${PC_LibOSRM_STATIC_LIBRARIES}" STREQUAL "")
-    set(LibOSRM_STATIC_LIBRARIES ${TEST_LibOSRM_STATIC_LIBRARY})
+    set(LibOSRM_STATIC_LIBRARIES ${TEST_LibOSRM_STATIC_LIBRARY} ${TEST_LibOSRM_STATIC_LDFLAGS})
   else()
-    set(LibOSRM_STATIC_LIBRARIES ${PC_LibOSRM_STATIC_LIBRARIES})
+    set(LibOSRM_STATIC_LIBRARIES ${PC_LibOSRM_STATIC_LIBRARIES} ${PC_LibOSRM_STATIC_LDFLAGS})
   endif()
   set(LibOSRM_LIBRARIES ${LibOSRM_STATIC_LIBRARIES})
 endif()
@@ -58,8 +67,9 @@ if (NOT ("${TEST_LibOSRM_DYNAMIC_LIBRARY}" STREQUAL "TEST_LibOSRM_DYNAMIC_LIBRAR
   set(LibOSRM_LIBRARIES ${LibOSRM_DYNAMIC_LIBRARIES})
 endif()
 
+
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LIBOSRM_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(LibOSRM DEFAULT_MSG
-                                LibOSRM_LIBRARIES LibOSRM_INCLUDE_DIR)
+                                LibOSRM_LIBRARY_DIRS LibOSRM_CXXFLAGS LibOSRM_LIBRARIES LibOSRM_INCLUDE_DIR)
